a5c4df2a57fb02c44eeb28b51d18ec99f319c478
Albert <jiuzheyanglianxiwo@gmail.com>
2024-09-17 09:51:40 +0800

    Update OP.2305.1.sh

--- OP.2305.1.sh
+++ OP.2305.1.sh
@@ -32,5 +32,5 @@ git clone https://github.com/kiddin9/luci-theme-edge.git package/luci-theme-edge
 
 # 添加passwall
 git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/passwall-packages
-git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall
+#git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall
 git clone https://github.com/xiaorouji/openwrt-passwall2.git package/passwall2
